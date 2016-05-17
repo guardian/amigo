b4d2d96e5f4b10dc1ab2235ce78e1a5d8423ec2d
Chris Birchall <chris.birchall@guardian.co.uk>
2016-05-17 15:22:40 +0100

    Support ANSI-coloured text in Packer logs

--- app/packer/PackerOutputParser.scala
+++ app/packer/PackerOutputParser.scala
@@ -2,14 +2,17 @@ package packer
 
 import models.{ MessagePart, AmiId }
 
+import scala.collection.mutable.ArrayBuffer
+import scala.util.matching.Regex.MatchIterator
+
 object PackerOutputParser {
 
   sealed abstract class PackerEvent
   case class UiOutput(logLevel: String, messageParts: List[MessagePart]) extends PackerEvent
   case class AmiCreated(amiId: AmiId) extends PackerEvent
 
-  val UiOutputRegex = """^\d+,,ui,(.*),(.*)$""".r
-  val AmiCreatedRegex = """^\d+,.*,artifact,\d+,id,[a-z0-9-]*:(.*)$""".r
+  private val UiOutputRegex = """^\d+,,ui,(.*),(.*)$""".r
+  private val AmiCreatedRegex = """^\d+,.*,artifact,\d+,id,[a-z0-9-]*:(.*)$""".r
 
   def parseLine(line: String): Option[PackerEvent] = line match {
     case UiOutputRegex(messageType, output) => Some(UiOutput(toLogLevel(messageType), parseUiOutput(output)))
@@ -24,13 +27,58 @@ object PackerOutputParser {
     case _ => "info"
   }
 
+  // Matches a piece of text wrapped in ANSI codes, e.g. "\u001B[0;32mHello I am green\u001B[0m"
+  private val AnsiColouredPart = "\u001B\\[0;(\\d{1,2})m(.*?)\u001B\\[0m".r
+
   private def parseUiOutput(output: String): List[MessagePart] = {
-    val substituted = output
+    val message = output
       .replaceAllLiterally("%!(PACKER_COMMA)", ",")
       .replaceAllLiterally("\\n", "\n")
       .replaceAllLiterally("\\r", "\r")
 
-    // TODO parse ANSI colours
-    List(MessagePart(substituted, "black"))
+    parseAnsiColouredString(message).toList
   }
+
+  /*
+  Note: while this is not a full-blown ANSI code parser, it should handle anything Packer throws at it.
+  Packer kindly terminates all coloured sections with a reset, which makes things slightly easier for us.
+   */
+  private def parseAnsiColouredString(message: String): Seq[MessagePart] = {
+    val it = AnsiColouredPart.findAllIn(message)
+    val parts = ArrayBuffer.empty[MessagePart]
+    var previousEndIndex = 0
+
+    while (it.hasNext) {
+      it.next()
+      if (it.start > previousEndIndex) {
+        // Add any non-coloured part between the end of the previous coloured part
+        // and the start of this one
+        parts += MessagePart(message.substring(previousEndIndex, it.start), "black")
+      }
+
+      val colourCode = it.group(1)
+      val colour = AnsiColours.getOrElse(colourCode, "black")
+      val text = it.group(2)
+      parts += MessagePart(text, colour)
+
+      previousEndIndex = it.end
+    }
+
+    if (previousEndIndex < message.length) {
+      // Add any non-coloured part after the final coloured part
+      parts += MessagePart(message.substring(previousEndIndex), "black")
+    }
+
+    parts
+  }
+
+  // Only handle the foreground colours
+  private val AnsiColours = Map(
+    "31" -> "red",
+    "32" -> "green",
+    "33" -> "yellow",
+    "34" -> "blue",
+    "35" -> "magenta",
+    "36" -> "cyan"
+  )
 }
--- test/packer/PackerOutputParserSpec.scala
+++ test/packer/PackerOutputParserSpec.scala
@@ -12,7 +12,6 @@ class PackerOutputParserSpec extends FlatSpec with Matchers {
     PackerOutputParser.parseLine("""1455355203,,ui,message,    ubuntu-wily-java8: Adding tag: "SourceAMI": "ami-cda312be"""") should be(
       Some(PackerOutputParser.UiOutput("info", List(MessagePart("""    ubuntu-wily-java8: Adding tag: "SourceAMI": "ami-cda312be"""", "black"))))
     )
-
   }
 
   it should "parse an AMI creation message" in {
@@ -23,6 +22,42 @@ class PackerOutputParserSpec extends FlatSpec with Matchers {
 
   it should "return None if it doesn't recognise the input" in {
     PackerOutputParser.parseLine("yolo") should be(None)
+  }
+
+  it should "parse a line of coloured Packer output" in {
+    PackerOutputParser.parseLine("123,,ui,message,\u001B[0;32mfoo\u001B[0m") should be(
+      Some(PackerOutputParser.UiOutput("info", List(
+        MessagePart("foo", "green")
+      )))
+    )
+    PackerOutputParser.parseLine("123,,ui,message,foo\u001B[0;32mbar\u001B[0m") should be(
+      Some(PackerOutputParser.UiOutput("info", List(
+        MessagePart("foo", "black"),
+        MessagePart("bar", "green")
+      )))
+    )
+    PackerOutputParser.parseLine("123,,ui,message,foo\u001B[0;32mbar\u001B[0mbaz") should be(
+      Some(PackerOutputParser.UiOutput("info", List(
+        MessagePart("foo", "black"),
+        MessagePart("bar", "green"),
+        MessagePart("baz", "black")
+      )))
+    )
+    PackerOutputParser.parseLine("123,,ui,message,    grid-xenial: \u001B[0;33m127.0.0.1\u001B[0m                  : \u001B[0;32mok\u001B[0m\u001B[0;32m=\u001B[0m\u001B[0;32m9\u001B[0m    \u001B[0;33mchanged\u001B[0m\u001B[0;33m=\u001B[0m\u001B[0;33m7\u001B[0m    unreachable=0    failed=0") should be(
+      Some(PackerOutputParser.UiOutput("info", List(
+        MessagePart("    grid-xenial: ", "black"),
+        MessagePart("127.0.0.1", "yellow"),
+        MessagePart("                  : ", "black"),
+        MessagePart("ok", "green"),
+        MessagePart("=", "green"),
+        MessagePart("9", "green"),
+        MessagePart("    ", "black"),
+        MessagePart("changed", "yellow"),
+        MessagePart("=", "yellow"),
+        MessagePart("7", "yellow"),
+        MessagePart("    unreachable=0    failed=0", "black")
+      )))
+    )
   }
 
 }
