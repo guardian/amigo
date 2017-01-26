guardian/amigo

commit 19bb150890a12f776ff0e5cfa26e2be943069cfa

Ensure a `.get` won't fail
Alas, I can't find an equivalent of  `transform` for a potentially failing op. Maybe with right-biased `Either` in 2.12, that might be a good PR for Play.

app/controllers/BaseImageController.scala
@@ -1,11 +1,9 @@
 package controllers
 
 import com.gu.googleauth.GoogleAuthConfig
-
 import data._
 import models._
-
-import play.api.data.Form
+import play.api.data.{ Form, Mapping }
 import play.api.data.Forms._
 import play.api.i18n.{ I18nSupport, MessagesApi }
 import play.api.mvc._
@@ -76,17 +74,23 @@ object BaseImageController {
 
     val editBaseImage = Form(tuple(
       "description" -> text(maxLength = 10000),
-      "amiId" -> nonEmptyText(maxLength = 16).transform[AmiId](AmiId.apply, _.value),
-      "linuxDist" -> nonEmptyText(maxLength = 16).transform[LinuxDist](LinuxDist.create(_).get, _.name)
+      "amiId" -> amiId,
+      "linuxDist" -> linuxDist
     ))
 
     val createBaseImage = Form(tuple(
       "id" -> text(maxLength = 50).transform[BaseImageId](BaseImageId.apply, _.value),
       "description" -> text(maxLength = 10000),
-      "amiId" -> nonEmptyText(maxLength = 16).transform[AmiId](AmiId.apply, _.value),
-      "linuxDist" -> nonEmptyText(maxLength = 16).transform[LinuxDist](LinuxDist.create(_).get, _.name)
+      "amiId" -> amiId,
+      "linuxDist" -> linuxDist
     ))
 
+    val amiId = nonEmptyText(maxLength = 16).transform[AmiId](AmiId.apply, _.value)
+
+    val linuxDist: Mapping[LinuxDist] =
+      nonEmptyText(maxLength = 16)
+        .verifying(s"Must be one of ${LinuxDist.all.keys}", LinuxDist.create(_).nonEmpty)
+        .transform(LinuxDist.create(_).get, _.name)
   }
 }
 

commit 5f9011ccc15692a3444044116a1f87b321a9dbef

Fix: Reference to uninitialized value amiId and linuxDist
The issue was introduced in 19bb150890a12f776ff0e5cfa26e2be943069cfa

app/controllers/BaseImageController.scala
@@ -72,6 +72,13 @@ object BaseImageController {
 
   object Forms {
 
+    val amiId = nonEmptyText(maxLength = 16).transform[AmiId](AmiId.apply, _.value)
+
+    val linuxDist: Mapping[LinuxDist] =
+      nonEmptyText(maxLength = 16)
+        .verifying(s"Must be one of ${LinuxDist.all.keys}", LinuxDist.create(_).nonEmpty)
+        .transform(LinuxDist.create(_).get, _.name)
+
     val editBaseImage = Form(tuple(
       "description" -> text(maxLength = 10000),
       "amiId" -> amiId,
@@ -84,13 +91,6 @@ object BaseImageController {
       "amiId" -> amiId,
       "linuxDist" -> linuxDist
     ))
-
-    val amiId = nonEmptyText(maxLength = 16).transform[AmiId](AmiId.apply, _.value)
-
-    val linuxDist: Mapping[LinuxDist] =
-      nonEmptyText(maxLength = 16)
-        .verifying(s"Must be one of ${LinuxDist.all.keys}", LinuxDist.create(_).nonEmpty)
-        .transform(LinuxDist.create(_).get, _.name)
   }
 }
 