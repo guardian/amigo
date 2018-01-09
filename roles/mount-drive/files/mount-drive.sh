72b841880f3d8cd52c7ae214f5be92596528efc2
Sam Cutler <sam.cutler@guardian.co.uk>
2018-01-09 14:01:23 +0000

    probably shouldnt overwrite the PATH variable

--- roles/mount-drive/files/mount-drive.sh
+++ roles/mount-drive/files/mount-drive.sh
@@ -2,12 +2,12 @@
 
 # Mount a drive
 DEVICE=$1
-PATH=$2
+MOUNT_PATH=$2
 
 if [ -e "$DEVICE" ]; then
-    mkdir $PATH
-    mkfs -t ext4 $DEVICE
-    echo "$DEVICE $PATH ext4 defaults 0 0" >> /etc/fstab
+    mkdir "$MOUNT_PATH"
+    mkfs -t ext4 "$DEVICE"
+    echo "$DEVICE" "$MOUNT_PATH" ext4 defaults 0 0" >> /etc/fstab
     mount -a
 else
     echo "Device $DEVICE does not exist" 1>&2
