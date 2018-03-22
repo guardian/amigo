b41ae87926f9b51def343cbdda39238e2066cc82
amyhughes <amy.hughes@guardian.co.uk>
2018-03-22 17:17:42 +0000

    amend path to hostnames file

--- roles/friendly-hostnames/files/set-hostname.sh
+++ roles/friendly-hostnames/files/set-hostname.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
-FRIENDLY_HOSTNAME=`iconv -f utf8 -t ascii//TRANSLIT /usr/share/hostnames | sed s/[^A-Za-z]*//g | awk 'length>4 && length<30' | shuf -n 1`
+FRIENDLY_HOSTNAME=`iconv -f utf8 -t ascii//TRANSLIT /opt/features/friendly-hostnames/hostnames.txt | sed s/[^A-Za-z]*//g | awk 'length>4 && length<30' | shuf -n 1`
 
 echo "127.0.1.1 $FRIENDLY_HOSTNAME" >> /etc/hosts
 echo $FRIENDLY_HOSTNAME > /etc/hostname
 hostname $FRIENDLY_HOSTNAME
-rm /usr/share/hostnames
+rm /opt/features/friendly-hostnames/hostnames.txt
 
 INSTANCE_ID="`wget -qO- http://instance-data/latest/meta-data/instance-id`"
 REGION="`wget -qO- http://instance-data/latest/meta-data/placement/availability-zone | sed -e 's:\([0-9][0-9]*\)[a-z]*\$:\\1:'`"
