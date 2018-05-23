f5e61c2f0617de39aa813695b6648aa6ee1f6812
Michael Barton <michael.barton@guardian.co.uk>
2018-05-23 11:17:46 +0200

    Exit immediately if we are unable to call describe-tags

--- roles/aws-await-tags/templates/await_tags.sh
+++ roles/aws-await-tags/templates/await_tags.sh
@@ -7,6 +7,11 @@ for i in {1..12}
 do
     NUM_TAGS=$(aws ec2 describe-tags --filters Name=resource-type,Values=instance Name=resource-id,Values=${INSTANCE_ID} --region eu-west-1 | jq ".Tags | length")
 
+    if [[ -z $NUM_TAGS ]]; then
+        echo "Unable to call describe tags. Exiting immediately"
+        exit 255
+    fi
+
     if (( $NUM_TAGS > 0 )); then
         echo "AWS tags loaded"
         exit 0
