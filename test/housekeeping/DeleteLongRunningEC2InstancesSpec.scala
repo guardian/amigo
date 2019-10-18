529a7eba4d9a107b975eac436d69151d0d542baf
Mullefa <gccdawson@gmail.com>
2019-10-18 15:47:52 +0100

    update test

--- test/housekeeping/DeleteLongRunningEC2InstancesSpec.scala
+++ test/housekeeping/DeleteLongRunningEC2InstancesSpec.scala
@@ -1,14 +1,17 @@
 package housekeeping
 
-import com.amazonaws.services.ec2.model.Instance
+import com.amazonaws.services.ec2.model.{ Instance, Tag }
 import housekeeping.utils.{ BakesRepo, PackerEC2Client }
+import models.{ BakeId, RecipeId }
 import org.joda.time.DateTime
 import org.mockito.ArgumentMatchers
 import org.mockito.Mockito._
 import org.scalatest.mock.MockitoSugar
-import org.scalatest.{ FlatSpec, Matchers }
+import org.scalatest.{ FlatSpec, Matchers, OptionValues }
 
-class DeleteLongRunningEC2InstancesSpec extends FlatSpec with Matchers with MockitoSugar {
+import scala.collection.JavaConversions._
+
+class DeleteLongRunningEC2InstancesSpec extends FlatSpec with Matchers with MockitoSugar with OptionValues {
 
   trait Mocks {
     val bakesRepo: BakesRepo = mock[BakesRepo]
@@ -17,7 +20,9 @@ class DeleteLongRunningEC2InstancesSpec extends FlatSpec with Matchers with Mock
   }
 
   "getBakeIdFromInstance" should "successfully parse a bake id from a tag" in {
-
+    val instance = mock[Instance]
+    when(instance.getTags).thenReturn(List(new Tag("BakeId", "recipe #2")))
+    DeleteLongRunningEC2Instances.getBakeIdFromInstance(instance).value shouldEqual BakeId(RecipeId("recipe"), 2)
   }
 
   "runHouseKeeping" should "terminate all instances running for over an hour" in new Mocks {
@@ -26,15 +31,15 @@ class DeleteLongRunningEC2InstancesSpec extends FlatSpec with Matchers with Mock
 
     val longRunningInstance1 = mock[Instance]
     when(longRunningInstance1.getLaunchTime).thenReturn(now.minusHours(2).toDate)
-    when(longRunningInstance1.getInstanceId).thenReturn("lr1")
+    when(longRunningInstance1.getInstanceId).thenReturn("1")
 
     val longRunningInstance2 = mock[Instance]
     when(longRunningInstance2.getLaunchTime).thenReturn(now.minusMinutes(61).toDate)
-    when(longRunningInstance2.getInstanceId).thenReturn("lr2")
+    when(longRunningInstance2.getInstanceId).thenReturn("2")
 
     val instance = mock[Instance]
     when(instance.getLaunchTime).thenReturn(now.minusMinutes(24).toDate)
-    when(instance.getInstanceId).thenReturn("r1")
+    when(instance.getInstanceId).thenReturn("3")
 
     when(packerEC2Client.getRunningPackerInstances()).thenReturn(
       List(longRunningInstance1, longRunningInstance2, instance)
@@ -42,8 +47,11 @@ class DeleteLongRunningEC2InstancesSpec extends FlatSpec with Matchers with Mock
 
     housekeepingJob.runHouseKeeping(now.minusHours(1))
 
-    verify(packerEC2Client, times(1)).terminateEC2Instance(ArgumentMatchers.eq("lr1"))
-    verify(packerEC2Client, times(1)).terminateEC2Instance(ArgumentMatchers.eq("lr2"))
-    verify(packerEC2Client, times(0)).terminateEC2Instance(ArgumentMatchers.eq("r1"))
+    // Check tha requests were made to delete long running instances.
+    verify(packerEC2Client, times(1)).terminateEC2Instance(ArgumentMatchers.eq("1"))
+    verify(packerEC2Client, times(1)).terminateEC2Instance(ArgumentMatchers.eq("2"))
+
+    // Check that a request wasn't made to delete the instance which isn't considered as long running.
+    verify(packerEC2Client, times(0)).terminateEC2Instance(ArgumentMatchers.eq("3"))
   }
 }
