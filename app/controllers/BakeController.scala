df2f7965b6bf9f0dee5bb2d597531b6d1ecd80c0
Philip McMahon <philip.mcmahon@guardian.co.uk>
2021-01-06 12:00:37 +0000

    More accurate error reporting

--- app/controllers/BakeController.scala
+++ app/controllers/BakeController.scala
@@ -48,7 +48,7 @@ class BakeController(
       val bakeLogs = BakeLogs.list(BakeId(recipeId, buildNumber))
       val packageList = amigoDataBucket
         .map(b => PackageList.getPackageList(s3Client, BakeId(recipeId, buildNumber), b))
-        .getOrElse(PackageList.unavailable)
+        .getOrElse(List("Amigo data bucket not defined: Package list unavailable"))
       Ok(views.html.showBake(bake, bakeLogs, packageList))
     }
   }
--- app/data/PackageList.scala
+++ app/data/PackageList.scala
@@ -8,8 +8,6 @@ import scala.util.control.NonFatal
 
 object PackageList extends Loggable {
 
-  val unavailable: List[String] = List("Package list unavailable - has the bake finished?")
-
   def removeNonPackageLines(packages: List[String]): List[String] = {
     packages.filter { p =>
       !(p.contains("Listing") || p.contains("Installed Packages") || p.contains("Loaded plugins"))
@@ -19,14 +17,13 @@ object PackageList extends Loggable {
   def getPackageList(s3Client: AmazonS3, bakeId: BakeId, bucket: String): List[String] = {
     val packageListKey = s"packagelists/${BakeId.toFilename(bakeId)}"
     try {
-      if (s3Client.doesObjectExist(bucket, packageListKey)) {
         val list = s3Client.getObjectAsString(bucket, packageListKey)
         removeNonPackageLines(list.split("\n").toList)
-      } else unavailable
     } catch {
       case NonFatal(e) =>
-        log.warn(s"Failed to fetch package list from S3 bucket $bucket, key $packageListKey", e)
-        unavailable
+        val message = s"Failed to fetch package list from S3 bucket $bucket, key $packageListKey"
+        log.warn(message, e)
+        List(message)
     }
 
 
