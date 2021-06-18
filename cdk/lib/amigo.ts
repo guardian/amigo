e8e02da0630edadc1d335edd51eac6ebfd070832
Akash Askoolum <akash1810@gmail.com>
2021-06-18 17:45:13 +0100

    feat: Define `SSMRunCommandPolicy` in the CDK stack
    Moving the `SSMRunCommandPolicy` resource into the CDK stack.
    
    This is chosen because it's fairly easy to test if it works: if successful, we can continue to connect to an instance using ssm-scala.
    
    AMIgo's stack defines two additional actions over the standard `GuSSMRunCommandPolicy`:
      1. ssm:StartSession
      2. ssm:TerminateSession
    
    Slightly annoyingly, `GuSSMRunCommandPolicy` is a singleton and doesn't accept additional input.
    Therefore, we have to add these actions into a new statement on the policy.
    
    `GuSSMRunCommandPolicy`'s `Roles` array is kinda hilarious (see snapshot test change).
    The good news is it's done by AWS CDK magic, so should work!

--- cdk/lib/amigo.ts
+++ cdk/lib/amigo.ts
@@ -1,8 +1,11 @@
 import path from "path";
+import type { CfnRole } from "@aws-cdk/aws-iam";
+import { Effect, PolicyStatement, Role } from "@aws-cdk/aws-iam";
 import { CfnInclude } from "@aws-cdk/cloudformation-include";
 import type { App } from "@aws-cdk/core";
 import type { GuStackProps, GuStageParameter } from "@guardian/cdk/lib/constructs/core";
 import { GuStack } from "@guardian/cdk/lib/constructs/core";
+import { GuSSMRunCommandPolicy } from "@guardian/cdk/lib/constructs/iam";
 
 const yamlTemplateFilePath = path.join(__dirname, "../../cloudformation.yaml");
 
@@ -10,11 +13,30 @@ export class AmigoStack extends GuStack {
   constructor(scope: App, id: string, props: GuStackProps) {
     super(scope, id, props);
 
-    new CfnInclude(this, "YamlTemplate", {
+    const yamlDefinedStack = new CfnInclude(this, "YamlTemplate", {
       templateFile: yamlTemplateFilePath,
       parameters: {
         Stage: this.getParam<GuStageParameter>("Stage"), // TODO `GuStageParameter` could be a singleton to simplify this
       },
     });
+
+    // See https://docs.aws.amazon.com/cdk/latest/guide/use_cfn_template.html#use_cfn_template_cfninclude_access
+    const cfnRootRole = yamlDefinedStack.getResource("RootRole") as CfnRole;
+    const rootRole = Role.fromRoleArn(this, "RootRole", cfnRootRole.attrArn);
+
+    const ssmPolicy = GuSSMRunCommandPolicy.getInstance(this);
+
+    // TODO Can GuSSMRunCommandPolicy expose its default `PolicyStatement` to easily add actions to it?
+    ssmPolicy.addStatements(
+      new PolicyStatement({
+        effect: Effect.ALLOW,
+        resources: ["*"],
+
+        // required to allow Packer to run SSM commands
+        // see https://github.com/guardian/amigo/pull/526 and https://github.com/guardian/amigo/pull/538
+        actions: ["ssm:StartSession", "ssm:TerminateSession"],
+      })
+    );
+    ssmPolicy.attachToRole(rootRole);
   }
 }
