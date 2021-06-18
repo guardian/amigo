d2c480c2820e20fe11d376519dd10a5c7c367b36
Akash Askoolum <akash1810@gmail.com>
2021-06-18 07:55:15 +0100

    feat: Include YAML template in the CDK stack
    Including the YAML template in the CDK stack should allow us to successfully use the CDK definition in CI/CD.

--- cdk/lib/amigo.ts
+++ cdk/lib/amigo.ts
@@ -1,9 +1,20 @@
+import path from "path";
+import { CfnInclude } from "@aws-cdk/cloudformation-include";
 import type { App } from "@aws-cdk/core";
-import type { GuStackProps } from "@guardian/cdk/lib/constructs/core";
+import type { GuStackProps, GuStageParameter } from "@guardian/cdk/lib/constructs/core";
 import { GuStack } from "@guardian/cdk/lib/constructs/core";
+
+const yamlTemplateFilePath = path.join(__dirname, "../../cloudformation.yaml");
 
 export class AmigoStack extends GuStack {
   constructor(scope: App, id: string, props: GuStackProps) {
-    super(scope, id, { ...props, description: "AMIgo, an AMI bakery" });
+    super(scope, id, props);
+
+    new CfnInclude(this, "YamlTemplate", {
+      templateFile: yamlTemplateFilePath,
+      parameters: {
+        Stage: this.getParam<GuStageParameter>("Stage"), // TODO `GuStageParameter` could be a singleton to simplify this
+      },
+    });
   }
 }
