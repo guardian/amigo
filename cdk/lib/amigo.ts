1395df47355c0d4cc4cbeef4be6a53a5efe57051
Akash Askoolum <akash1810@gmail.com>
2021-06-23 14:07:49 +0100

    refactor: Reduce the number of individual calls to `attachToRole`

--- cdk/lib/amigo.ts
+++ cdk/lib/amigo.ts
@@ -1,5 +1,5 @@
 import path from "path";
-import type { CfnRole } from "@aws-cdk/aws-iam";
+import type { CfnRole, Policy } from "@aws-cdk/aws-iam";
 import { Effect, PolicyStatement, Role } from "@aws-cdk/aws-iam";
 import { CfnInclude } from "@aws-cdk/cloudformation-include";
 import type { App } from "@aws-cdk/core";
@@ -53,54 +53,54 @@ export class AmigoStack extends GuStack {
         actions: ["ssm:StartSession", "ssm:TerminateSession"],
       })
     );
-    ssmPolicy.attachToRole(rootRole);
 
-    GuLogShippingPolicy.getInstance(this).attachToRole(rootRole);
+    const policiesToAttachToRootRole: Policy[] = [
+      ssmPolicy,
+      GuLogShippingPolicy.getInstance(this),
+      new GuGetDistributablePolicy(this, AmigoStack.app),
+      new GuAllowPolicy(this, "PackerPolicy", {
+        policyName: "packer-required-permissions",
+        resources: ["*"],
+        actions: [
+          "ec2:AttachVolume",
+          "ec2:AuthorizeSecurityGroupIngress",
+          "ec2:CopyImage",
+          "ec2:CreateImage",
+          "ec2:CreateKeypair",
+          "ec2:CreateSecurityGroup",
+          "ec2:CreateSnapshot",
+          "ec2:CreateTags",
+          "ec2:CreateVolume",
+          "ec2:DeleteKeypair",
+          "ec2:DeleteSecurityGroup",
+          "ec2:DeleteSnapshot",
+          "ec2:DeleteVolume",
+          "ec2:DeregisterImage",
+          "ec2:DescribeImageAttribute",
+          "ec2:DescribeImages",
+          "ec2:DescribeInstances",
+          "ec2:DescribeRegions",
+          "ec2:DescribeSecurityGroups",
+          "ec2:DescribeSnapshots",
+          "ec2:DescribeSubnets",
+          "ec2:DescribeTags",
+          "ec2:DescribeVolumes",
+          "ec2:DetachVolume",
+          "ec2:GetPasswordData",
+          "ec2:ModifyImageAttribute",
+          "ec2:ModifyInstanceAttribute",
+          "ec2:ModifySnapshotAttribute",
+          "ec2:RegisterImage",
+          "ec2:RunInstances",
+          "ec2:StopInstances",
+          "ec2:TerminateInstances",
+          "iam:PassRole",
+        ],
+      }),
+      GuDescribeEC2Policy.getInstance(this),
+      GuAnghammaradSenderPolicy.getInstance(this),
+    ];
 
-    new GuGetDistributablePolicy(this, AmigoStack.app).attachToRole(rootRole);
-
-    new GuAllowPolicy(this, "PackerPolicy", {
-      policyName: "packer-required-permissions",
-      resources: ["*"],
-      actions: [
-        "ec2:AttachVolume",
-        "ec2:AuthorizeSecurityGroupIngress",
-        "ec2:CopyImage",
-        "ec2:CreateImage",
-        "ec2:CreateKeypair",
-        "ec2:CreateSecurityGroup",
-        "ec2:CreateSnapshot",
-        "ec2:CreateTags",
-        "ec2:CreateVolume",
-        "ec2:DeleteKeypair",
-        "ec2:DeleteSecurityGroup",
-        "ec2:DeleteSnapshot",
-        "ec2:DeleteVolume",
-        "ec2:DeregisterImage",
-        "ec2:DescribeImageAttribute",
-        "ec2:DescribeImages",
-        "ec2:DescribeInstances",
-        "ec2:DescribeRegions",
-        "ec2:DescribeSecurityGroups",
-        "ec2:DescribeSnapshots",
-        "ec2:DescribeSubnets",
-        "ec2:DescribeTags",
-        "ec2:DescribeVolumes",
-        "ec2:DetachVolume",
-        "ec2:GetPasswordData",
-        "ec2:ModifyImageAttribute",
-        "ec2:ModifyInstanceAttribute",
-        "ec2:ModifySnapshotAttribute",
-        "ec2:RegisterImage",
-        "ec2:RunInstances",
-        "ec2:StopInstances",
-        "ec2:TerminateInstances",
-        "iam:PassRole",
-      ],
-    }).attachToRole(rootRole);
-
-    GuDescribeEC2Policy.getInstance(this).attachToRole(rootRole);
-
-    GuAnghammaradSenderPolicy.getInstance(this).attachToRole(rootRole);
+    policiesToAttachToRootRole.forEach((policy) => policy.attachToRole(rootRole));
   }
 }
