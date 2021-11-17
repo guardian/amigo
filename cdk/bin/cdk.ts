b7816ecf34c62417f2c1c158b5254bb57405b0f1
Jacob Winch <jacob.winch@guardian.co.uk>
2021-11-17 13:33:11 +0000

    Use @guardian/cdk 31.0.0

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,12 +1,12 @@
 import { App } from "@aws-cdk/core";
 import { AmigoStack } from "../lib/amigo";
 
-const stackName = process.env.GU_CDK_STACK_NAME;
+const cloudFormationStackName = process.env.GU_CFN_STACK_NAME;
 
 const app = new App();
 
 new AmigoStack(app, "AMIgo", {
   stack: "deploy",
   migratedFromCloudFormation: true,
-  stackName,
+  cloudFormationStackName,
 });
