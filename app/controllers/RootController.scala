caa96dbb119fbb5b8f63d6e8a0e95ed6d3f114f9
Akash Askoolum <akash1810@users.noreply.github.com>
2021-11-24 15:25:31 +0000

    Update RootController.scala

--- app/controllers/RootController.scala
+++ app/controllers/RootController.scala
@@ -4,11 +4,13 @@ import amigo.BuildInfo
 import com.gu.googleauth.GoogleAuthConfig
 import play.api.libs.json.Json
 import play.api.mvc._
+import services.Loggable
+import java.time.LocalDate
 
-class RootController(val authConfig: GoogleAuthConfig) extends Controller with AuthActions {
+class RootController(val authConfig: GoogleAuthConfig) extends Controller with AuthActions with Loggable {
 
   def healthcheck = Action {
-    println("hi")
+    log.info(LocalDate.now)
     Ok(Json.parse(BuildInfo.toJson))
   }
 
