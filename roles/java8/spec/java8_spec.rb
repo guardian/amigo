f427587fa19feb801688870c57be32a49758e011
Jon Soul <jonathan.soul@guardian.co.uk>
2021-12-16 10:36:23 +0000

    Draft: add java agent for removing

--- roles/java8/spec/java8_spec.rb
+++ roles/java8/spec/java8_spec.rb
@@ -2,3 +2,7 @@
 describe command('java -version') do
   its(:stdout) { should match /java version "1\.8.*/ }
 end
+
+describe command('echo $JAVA_TOOL_OPTIONS') do
+  its(:stdout) { should match /-javaagent:\/usr\/local\/log4j-hotpatch\/Log4JHotpatch\.jar/ }
+end
