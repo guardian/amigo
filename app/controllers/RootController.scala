cefb5a3889b3aeb0b05ec019a44763228e4524e6
Jacob Winch <jacob.winch@guardian.co.uk>
2021-12-16 10:03:49 +0000

    Add simple logging for demo

--- app/controllers/RootController.scala
+++ app/controllers/RootController.scala
@@ -4,10 +4,12 @@ import amigo.BuildInfo
 import com.gu.googleauth.GoogleAuthConfig
 import play.api.libs.json.Json
 import play.api.mvc._
+import services.Loggable
 
-class RootController(val authConfig: GoogleAuthConfig) extends Controller with AuthActions {
+class RootController(val authConfig: GoogleAuthConfig) extends Controller with AuthActions with Loggable {
 
   def healthcheck = Action {
+    log.info("Developer Tooling Intro - demo")
     Ok(Json.parse(BuildInfo.toJson))
   }
 
