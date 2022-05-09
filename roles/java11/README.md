60b275e7af47040c8664cc0ae83e4d9f84ac038f
Nicolas Long <nicolas.long@theguardian.com>
2022-05-09 14:18:43 +0100

    Deprecate openjdk roles
    The Java 17 (openjdk) role has been completely removed as it is
    unused.
    
    Corretto is the preferred distribution now.
    
    For rationale, see:
    
    https://docs.google.com/document/d/1ZR-YnaXCT5_gLVmTCeGs0mWd3KPaAozPjQK8uUzHZ9w/edit#heading=h.bylq0fpat69m

---
+++ roles/java11/README.md
@@ -0,0 +1,3 @@
+# Java 11 (openjdk)
+
+**This role is now DEPRECATED. Switch to the `java11corretto` role.**
--- roles/java17/spec/java17_spec.rb
+++
@@ -1,4 +0,0 @@
-
-describe command('java -version') do
-  its(:stdout) { should match /java version "17\..*/ }
-end
