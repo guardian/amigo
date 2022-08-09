35d17de838286f269698a38f852fcd0865a407d6
Jon Soul <jonathan.soul@guardian.co.uk>
2022-08-09 15:27:46 +0100

    Revert "Use v2 credentials for simple-configuration"
    This reverts commit c78537396245c0e32376dbb9f7af56d8ed4d77fa.

--- app/AppLoader.scala
+++ app/AppLoader.scala
@@ -1,13 +1,10 @@
-import com.amazonaws.auth.profile.ProfileCredentialsProvider
-import com.amazonaws.auth.{AWSCredentialsProviderChain, InstanceProfileCredentialsProvider}
-import com.gu.conf.{ConfigurationLoader, SSMConfigurationLocation}
-import com.gu.{AppIdentity, AwsIdentity}
+import com.gu.{ AppIdentity, AwsIdentity }
+import com.gu.conf.{ ConfigurationLoader, SSMConfigurationLocation }
+import play.api.libs.logback.LogbackLoggerConfigurator
+import play.api.{ Application, ApplicationLoader, Configuration }
 import components.AppComponents
 import play.api.ApplicationLoader.Context
-import play.api.libs.logback.LogbackLoggerConfigurator
-import play.api.{Application, ApplicationLoader, Configuration}
 import services.Loggable
-import software.amazon.awssdk.auth.credentials.{AwsCredentialsProviderChain => AwsCredentialsProviderChainV2, InstanceProfileCredentialsProvider => InstanceProfileCredentialsProviderV2, ProfileCredentialsProvider => ProfileCredentialsProviderV2}
 
 import scala.concurrent.Future
 
@@ -15,25 +12,12 @@ class AppLoader extends ApplicationLoader with Loggable {
   override def load(context: Context): Application = {
     new LogbackLoggerConfigurator().configure(context.environment)
     try {
-      val awsCredsForV1 = new AWSCredentialsProviderChain(
-        new ProfileCredentialsProvider("deployTools"),
-        new ProfileCredentialsProvider(),
-        InstanceProfileCredentialsProvider.getInstance())
-
-      val awsCredsForV2 = AwsCredentialsProviderChainV2
-        .builder()
-        .credentialsProviders(
-          ProfileCredentialsProviderV2.create("deployTools"),
-          ProfileCredentialsProviderV2.create(),
-          InstanceProfileCredentialsProviderV2.create())
-        .build()
-
-      val identity = AppIdentity.whoAmI(defaultAppName = "amigo", awsCredsForV2).get
+      val identity = AppIdentity.whoAmI(defaultAppName = "amigo")
       val loadedConfig = ConfigurationLoader.load(identity) {
         case identity: AwsIdentity => SSMConfigurationLocation.default(identity)
       }
       val newContext = context.copy(initialConfiguration = Configuration(loadedConfig).withFallback(context.initialConfiguration))
-      val components = new AppComponents(newContext, identity, awsCredsForV1, awsCredsForV2)
+      val components = new AppComponents(newContext, identity)
       log.info("Starting the scheduler")
       components.quartzScheduler.start()
       components.applicationLifecycle.addStopHook { () =>
