02e58ef53edf08a849f63a478f65df7f7f8a09de
Jacob Winch <jacob.winch@guardian.co.uk>
2023-03-09 08:55:22 +0000

    Revert "Prevent AMIgo from upgrading grub-efi-arm64 packages on Ubuntu 18 (#1037)"
    This reverts commit 489bbb1d2980c460ee303c577610511b86ef914e.

--- app/ansible/PlaybookGenerator.scala
+++ app/ansible/PlaybookGenerator.scala
@@ -11,12 +11,6 @@ object PlaybookGenerator {
       |
       |- hosts: all
       |  become: yes
-      |  pre_tasks:
-      |    - name: Prevent apt from upgrading grub-efi-arm64-signed
-      |      ansible.builtin.dpkg_selections:
-      |        name: grub-efi-arm64-signed
-      |        selection: hold
-      |      when: ansible_facts['distribution_major_version'] == "18" and ansible_facts['architecture'] == "aarch64"
       |  vars:
       |${allVars.map { case (k, v) => s"    $k: $v" }.mkString("\n")}
       |  roles:
