da20062f86f2af3c2b6451200808b33444b6a4d3
Kelvin Chappell <kelvin.chappell@theguardian.com>
2024-12-13 22:11:37 +0000

    Stop at direct dependencies

--- deps/src/main/scala/deps/Trial.scala
+++ deps/src/main/scala/deps/Trial.scala
@@ -4,15 +4,24 @@ import io.circe.Json
 import io.circe.optics.JsonPath.root
 import io.circe.parser.parse
 
+import scala.math.Ordering.Implicits.seqOrdering
+
 object Trial {
+
+  case class Dependency(
+      name: String,
+      scope: Option[String],
+      direct: Option[Boolean]
+  )
+
   def main(args: Array[String]): Unit = {
 
-    val dep = "com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.14.3"
+//    val dep = "com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.14.3"
+//    val dep = "org.playframework:play-guice_2.13:3.0.6"
+    val dep = "org.playframework:play-ws-standalone_2.13:3.0.6"
 
     val snapshot = scala.io.Source
-      .fromFile(
-        "/Users/Kelvin_Chappell/code/amigo/deps/src/main/scala/deps/snapshot.json"
-      )
+      .fromFile("deps/src/main/scala/deps/snapshot.json")
       .mkString
 
     val json = parse(snapshot) match {
@@ -25,34 +34,56 @@ object Trial {
       .flatMap(_.asObject.map(_.toMap).getOrElse(Map.empty))
       .toMap
 
-    def f(s: String): List[List[String]] = {
+    def f(s: Dependency): List[List[Dependency]] = {
 
       def reversePaths(
-          current: String,
-          path: List[String]
-      ): List[List[String]] = {
-        val parents = resolved.filter { case (_, children) =>
-          root.dependencies.each.string.getAll(children).contains(current)
-        }.keys
+          current: Dependency,
+          path: List[Dependency]
+      ): List[List[Dependency]] = {
 
-        if (parents.isEmpty) {
-          // If there are no parents, we've reached the end of a reverse path
+        if (current.direct.getOrElse(false)) {
+          // If direct dependency, we've reached the end of a reverse path
           List(current :: path)
         } else {
-          // Otherwise, continue finding reverse paths for each parent
-          parents.toList.flatMap(parent =>
-            reversePaths(parent, current :: path)
-          )
+          val parents = resolved
+            .filter { case (_, children) =>
+              root.dependencies.each.string
+                .getAll(children)
+                .contains(current.name)
+            }
+            .map { case (k, v) =>
+              Dependency(
+                k,
+                root.metadata.config.string.getOption(v),
+                root.relationship.string.getOption(v).map(_ == "direct")
+              )
+            }
+
+          if (parents.isEmpty) {
+            // If there are no parents, we've reached the end of a reverse path
+            List(current :: path)
+          } else {
+            // Otherwise, continue finding reverse paths for each parent
+            parents.toList.flatMap(parent =>
+              reversePaths(parent, current :: path)
+            )
+          }
         }
       }
 
       reversePaths(s, Nil)
     }
 
-    val packages2: List[List[String]] = f(dep)
+    val packages: List[List[Dependency]] = f(Dependency(dep, None, None))
+    // dedup list
+    val packages2 = packages.distinct.sortBy(x => x.map(_.name))
 
     for (p <- packages2) {
-      println(p.reverse.mkString(" > \n"))
+      for (d <- p) {
+        println(
+          s"${d.name} (${d.scope.getOrElse("unknown")}, ${d.direct.getOrElse("unknown")}) >"
+        )
+      }
       println
     }
   }
