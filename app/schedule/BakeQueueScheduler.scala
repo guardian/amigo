ebcb1ecee0f25d8f69996177f29802e265872bb4
Andrew Nowak <andrew.nowak@guardian.co.uk>
2025-08-15 16:57:25 +0100

    assign build number when queueing

--- app/schedule/BakeQueueScheduler.scala
+++ app/schedule/BakeQueueScheduler.scala
@@ -3,6 +3,7 @@ package schedule
 import data.{Dynamo, Recipes}
 import org.apache.pekko.actor.Scheduler
 import play.api.libs.json.Json
+import services.Loggable
 import software.amazon.awssdk.services.sqs.SqsClient
 import software.amazon.awssdk.services.sqs.model.SendMessageRequest
 
@@ -20,13 +21,14 @@ import scala.concurrent.ExecutionContext
 import scala.concurrent.duration._
 import scala.jdk.DurationConverters._
 
-object BakeQueueScheduler {
+object BakeQueueScheduler extends Loggable {
   private val London = ZoneId.of("Europe/London")
 
   // FIXME currently relies on there usually only being one instance of Amigo to avoid duplicating bakes unnecessarily...
   // Needs improving. Lambda on a schedule makes some sense, but then that's another service to deploy, with a dependency
   // chain to keep up-to-date, configuring an AWS SDK to scan the table and write to the queue, etc. It is much easier to
   // have an extra ≤20 lines of Scala running on the instance we already have...
+  // Maybe we have the actual bake runner skip a scheduled bake if one has run in the past 24h?
   def schedule(
       scheduler: Scheduler,
       sqsClient: SqsClient,
@@ -54,14 +56,21 @@ object BakeQueueScheduler {
 
       for {
         recipe <- todaysRecipes
-        bakeJob = BakeQueueJob(recipe)
       } {
-        val smr = SendMessageRequest
-          .builder()
-          .queueUrl(bakeQueueUrl)
-          .messageBody(Json.toJson(bakeJob).toString())
-          .build()
-        sqsClient.sendMessage(smr)
+        Recipes.incrementAndGetBuildNumber(recipe) match {
+          case Some(buildNo) =>
+            val bakeJob = BakeQueueJob(recipe, buildNo)
+            val smr = SendMessageRequest
+              .builder()
+              .queueUrl(bakeQueueUrl)
+              .messageBody(Json.toJson(bakeJob).toString())
+              .build()
+            sqsClient.sendMessage(smr)
+          case None =>
+            log.warn(
+              s"Failed to queue bake job for recipe $recipe - could not determine build number"
+            )
+        }
       }
     }
   }
